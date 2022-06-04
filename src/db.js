ef5fa41e8c9f5930653457b19ee80034a0cac8bb
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-06-04 14:02:30 -0400

    commenting out the db&server to test if everything is working

--- src/db.js
+++ src/db.js
@@ -1,37 +1,37 @@
-const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:admin@localhost/fair_play_react_router_db');
+// const Sequelize = require('sequelize');
+// const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:admin@localhost/fair_play_react_router_db');
 
-const Room = conn.define('room', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      notEmpty: true
-    }
-  },
-  courtNumber : {
-    default: 0,
-    type: Sequelize.INTEGER,
-  },
-  numberOfPlayers: {
-    default: 0,
-    type: Sequelize.INTEGER,
-  }
-})
+// const Room = conn.define('room', {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     unique: true,
+//     validate: {
+//       notEmpty: true
+//     }
+//   },
+//   courtNumber : {
+//     default: 0,
+//     type: Sequelize.INTEGER,
+//   },
+//   numberOfPlayers: {
+//     default: 0,
+//     type: Sequelize.INTEGER,
+//   }
+// })
 
+// // 
+// const syncAndSeed = async () => {
+//   await conn.sync({ force: true });
+//   const [pier6, centralPark] = await Promise.all([
+//     Room.create({ name: 'pier6' }),
+//     Room.create({ name: 'centralPark', courtNumber: 1, numberOfPlayers: 2 }),
+//   ])
+// }
 
-const syncAndSeed = async () => {
-  await conn.sync({ force: true });
-  const [pier6, centralPark] = await Promise.all([
-    Room.create({ name: 'pier6' }),
-    Room.create({ name: 'centralPark', courtNumber: 1, numberOfPlayers: 2 }),
-  ])
-}
-
-module.exports = {
-  syncAndSeed,
-  models: {
-    Room
-  }
-}
+// module.exports = {
+//   syncAndSeed,
+//   models: {
+//     Room
+//   }
+// }
